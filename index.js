274be5ef2fa866a8ca428ffecb71e5e03897e87f
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-10 13:49:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog';
+var animal = 'dog'
 
 function myAnimal() {
   return animal
@@ -9,15 +9,15 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   var animal = 'cat';
-  return animal;
+  return animal
 }
 
 function add2(n) {
-   const two = 2;
-   return n + two;
+   const two = 2
+  return n + two
 
   // Feel free to move things around!
-
+  
 }
 
 var funkyFunction = function() {
